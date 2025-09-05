bfcea904ee62d32bfbd9b533ccbb9031a8f368f2
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2025-09-05 20:56:52 +0200

    a bit more

--- common/src/main/java/com/lukasabbe/bookshelfinspector/data/BookData.java
+++ common/src/main/java/com/lukasabbe/bookshelfinspector/data/BookData.java
@@ -3,17 +3,26 @@ package com.lukasabbe.bookshelfinspector.data;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemStack;
 
+/**
+ * Data for a slot in a chiseled bookshelf
+ */
 public class BookData {
     public ItemStack itemStack;
     public BlockPos pos;
     public int slotId;
 
+    /**
+     * Creates data object with its data
+     */
     public BookData(ItemStack itemStack, BlockPos pos, int slotId) {
         this.itemStack = itemStack;
         this.pos = pos;
         this.slotId = slotId;
     }
 
+    /**
+     * Gets an empty BookData
+     */
     public static BookData empty(){
         return new BookData(ItemStack.EMPTY, null, -1);
     }
--- common/src/main/java/com/lukasabbe/bookshelfinspector/data/Tags.java
+++ common/src/main/java/com/lukasabbe/bookshelfinspector/data/Tags.java
@@ -5,6 +5,9 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
+/**
+ * Tags for blocks used in the mod
+ */
 public class Tags {
     public static final TagKey<Block> CHISELED_BOOKSHELVES = TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("c", "chiseled_bookshelves"));
     public static final TagKey<Block> LECTERNS = TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("c", "lectern"));
