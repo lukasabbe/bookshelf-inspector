5bd47af6af4388c48eb09db82fc078c110b1cddc
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2025-11-21 09:36:23 +0100

    1.21.11

--- common/src/main/java/com/lukasabbe/bookshelfinspector/data/Tags.java
+++ common/src/main/java/com/lukasabbe/bookshelfinspector/data/Tags.java
@@ -1,12 +1,12 @@
 package com.lukasabbe.bookshelfinspector.data;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
 public class Tags {
-    public static final TagKey<Block> CHISELED_BOOKSHELVES = TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("c", "chiseled_bookshelves"));
-    public static final TagKey<Block> LECTERNS = TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("c", "lectern"));
-    public static final TagKey<Block> SHELVES = TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("c", "shelves"));
+    public static final TagKey<Block> CHISELED_BOOKSHELVES = TagKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath("c", "chiseled_bookshelves"));
+    public static final TagKey<Block> LECTERNS = TagKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath("c", "lectern"));
+    public static final TagKey<Block> SHELVES = TagKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath("c", "shelves"));
 }
